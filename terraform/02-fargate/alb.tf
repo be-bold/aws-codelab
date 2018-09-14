679a19b3f06963dd1bd73775a3a4905bf5f46f0a
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-14 12:11:50 +0200

    Create ALB without connection to VPC yet

---
+++ terraform/02-fargate/alb.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "alb" {
+  name = "${local.basename}-alb"
+  vpc_id = ""
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0 // all
+    to_port = 0 // all
+    protocol = "-1" // all
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_lb" "this" {
+  name = "${local.basename}-alb"
+  internal = false
+  load_balancer_type = "application"
+  subnets = []
+  security_groups = ["${aws_security_group.alb.id}"]
+  tags = "${local.default_tags}"
+}
