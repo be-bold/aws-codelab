421fe83cacfd22306a0e831f0d71b01e05f6ec89
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-16 20:07:42 +0200

    Add script to setup development (env vars)

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# aws-codelab
+aws-codelab
+---
+
+# Start developing
+- copy env-vars.sh.template to env-vars.sh and define variables: `cp env-vars.sh.template env-vars.sh`
+- env-vars.sh is not checked-in
+- in each new shell run `source setup-development.sh` to set environment variables used by CI scripts
+
+# Service1
+- build and upload docker image: `./ci/build-and-upload-service1.sh`
 
 # ECR
 - Login: `$(aws ecr get-login --no-include-email --profile <profile>)`
---
+++ setup-development.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+###### Environment Variable Definitions ######
+
+source env-vars.sh
+
+
+###### Checks ######
+
+echo "Check if environment variables are set"
+
+echo "AWS_REGION (used by AWS SDKs, CLI and other tools): $AWS_REGION"
+if [[ -z "${AWS_REGION}" ]]; then
+    echo "ERROR: Environment variable AWS_REGION (e.g. eu-central-1 for Frankfurt) must be set!"
+    exit 1
+fi
+
+echo "AWS_PROFILE (used by AWS SDKs, CLI and other tools): $AWS_PROFILE"
+if [[ -z "${AWS_PROFILE}" ]]; then
+    echo "Environment variable AWS_PROFILE not set. Using 'default' as profile."
+    export AWS_PROFILE=default
+fi
+
+echo "AWS_ACCOUNT_ID (used just by us): $AWS_ACCOUNT_ID"
+if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
+    echo "ERROR: Environment variable AWS_ACCOUNT_ID must be set!"
+    exit 1
+fi
+
+
+echo "OK, lets start"
