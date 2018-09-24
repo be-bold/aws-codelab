dfebff01d1b99ac47a629a6058b3d1d23a976e5b
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-24 23:16:25 +0200

    Add basic terraform example for first task

---
+++ terraform/10-basics/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "eu-central-1"
+  profile = "codelab"
+  version = "1.37"
+}
+
+resource "aws_ssm_parameter" "my_parameter1" {
+  name = "team1-key1"
+  type = "String"
+  value = "team1-value"
+}
+
+
+
+// Add some variables, local values and outputs
+
+variable "parameter2_value" {
+  description = "value of the parameter"
+  type = "string"
+  default = "my-default-value"
+}
+
+locals {
+  team = "team1"
+
+  default_tags = {
+    team = "${local.team}"
+  }
+}
+
+resource "aws_ssm_parameter" "my_parameter2" {
+  name = "${local.team}-key2"
+  type = "String"
+  value = "${var.parameter2_value}"
+  tags = "${local.default_tags}"
+}
+
+output "parameter2_arn" {
+  value = "${aws_ssm_parameter.my_parameter2.arn}"
+}
