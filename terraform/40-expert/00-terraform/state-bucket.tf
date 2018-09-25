3f62e39077f6abcb3105e95a708a922167ebf38a
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-25 14:08:51 +0200

    Use kms alias instead of id for s3 bucket
    It is easier to read.

--- terraform/40-expert/00-terraform/state-bucket.tf
+++ terraform/40-expert/00-terraform/state-bucket.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "this" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.this.arn}"
+        kms_master_key_id = "${aws_kms_alias.this.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
