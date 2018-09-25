52925a4afcc6fe9f71b424fc672bdba3348da3b0
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-25 14:58:15 +0200

    Use s3 as backend with encrypt = false to avoid AWS usage

--- terraform/30-advanced/10-vpc/main.tf
+++ terraform/30-advanced/10-vpc/main.tf
@@ -10,6 +10,7 @@ terraform {
     profile = "codelab"
     bucket = "biz-kommitment-advanced-team1-terraform-state-eu-central-1"
     key = "products/vpc.tfstate"
-    encrypt = true
+    // if you use s3 bucket encryption, set encrypt = false to avoid encryption with AES instead of your KMS key
+    encrypt = false
   }
 }
--- terraform/40-expert/20-fargate-service1/main.tf
+++ terraform/40-expert/20-fargate-service1/main.tf
@@ -6,7 +6,8 @@ terraform {
     // uses AWS_PROFILE and AWS_DEFAULT_REGION
     bucket = "biz-kommitment-team1-terraform-state-eu-central-1"
     key = "products/fargate.tfstate"
-    encrypt = true
+    // if you use s3 bucket encryption, set encrypt = false to avoid encryption with AES instead of your KMS key
+    encrypt = false
     dynamodb_table = "team1-terraform-state-lock"
   }
 }
