fbbf9020af36b811f60b3fb7088ba51f918854f6
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-25 11:21:32 +0200

    Use hard-coded region in tf backend

--- terraform/30-advanced/10-vpc/main.tf
+++ terraform/30-advanced/10-vpc/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    region = "${local.region}"
+    region = "eu-central-1"
     profile = "codelab"
     bucket = "biz-kommitment-advanced-team1-terraform-state-eu-central-1"
     key = "products/vpc.tfstate"
