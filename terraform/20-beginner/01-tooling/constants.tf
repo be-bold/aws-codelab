b61a98cae0e9e93dddfb18cd531717a27c41fd49
Marcel Boettcher <marcel.boettcher@otto.de>
2018-09-25 07:52:08 +0200

    Add tooling (ecr) to beginner task

---
+++ terraform/20-beginner/01-tooling/constants.tf
@@ -0,0 +1,10 @@
+locals {
+
+  team_name = "team1"
+
+  default_tags = {
+    team = "${local.team_name}"
+    environment = "tooling"
+  }
+
+}
---
+++ terraform/20-beginner/01-tooling/init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# exit on error even if parts of a pipe fail
+set -e -o pipefail
+
+terraform init
